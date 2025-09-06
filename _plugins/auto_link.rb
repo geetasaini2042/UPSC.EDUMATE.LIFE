4e2d37bad9152c2b495572d1a92d39054f5942b5
geetasaini2042 <Geetasaini2042@gmail.com>
2025-09-06 20:18:54 +0530

    Update auto_link.rb

--- _plugins/auto_link.rb
+++ _plugins/auto_link.rb
@@ -1,17 +1,26 @@
 module Jekyll
   module AutoLinkFilter
-    def auto_link(input, posts)
+    def auto_link(input, posts, current_post_url = nil)
       posts.each do |post|
         next unless post.data['words']
+
+        # उसी post में खुद का link न बने
+        next if post.url == current_post_url
 
         post.data['words'].each do |word|
           url = post.url
 
-          # Match word only in plain text, not inside existing tags
+          # regex: avoid replacing inside tags
           regex = /(?<!["'>])\b#{Regexp.escape(word)}\b(?!["'<])/
 
+          linked_once = false
           input = input.gsub(regex) do |match|
-            "<a href='#{url}'>#{match}</a>"
+            if linked_once
+              match  # बाद की बार replace मत करो
+            else
+              linked_once = true
+              "<a href='#{url}'>#{match}</a>"
+            end
           end
         end
       end
