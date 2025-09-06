db08e7a25a14a5bd058b7dd3ef3bc7ffc0e23ea7
geetasaini2042 <Geetasaini2042@gmail.com>
2025-09-06 20:04:24 +0530

    Update auto_link.rb

--- _plugins/auto_link.rb
+++ _plugins/auto_link.rb
@@ -2,13 +2,15 @@ module Jekyll
   module AutoLinkFilter
     def auto_link(input, posts)
       posts.each do |post|
-        if post.data['words']
-          post.data['words'].each do |word|
-            url = post.url
-            regex = /\b#{Regexp.escape(word)}\b/
-            input = input.gsub(regex) do |match|
-              "<a href='#{url}'>#{match}</a>"
-            end
+        next unless post.data['words']
+
+        post.data['words'].each do |word|
+          url = post.url
+          # केवल plain शब्द को ही replace करो, लिंक के अंदर नहीं
+          regex = /(?<!href=['"])#{Regexp.escape(word)}(?!['"])/
+
+          input = input.gsub(regex) do |match|
+            "<a href='#{url}'>#{match}</a>"
           end
         end
       end
