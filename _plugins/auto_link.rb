5c6b31c3d16ba885a860c613e6748441ba4494bb
geetasaini2042 <Geetasaini2042@gmail.com>
2025-09-07 11:18:20 +0530

    Update auto_link.rb

--- _plugins/auto_link.rb
+++ _plugins/auto_link.rb
@@ -1,34 +1,46 @@
 module Jekyll
   module AutoLinkFilter
     def auto_link(input, posts, current_post_url = nil)
+      # पहले सभी words को length के हिसाब से sort करो (लंबे पहले)
+      words_with_urls = []
+
       posts.each do |post|
         next unless post.data['words']
         next if post.url == current_post_url   # खुद के post में link मत लगाओ
 
         post.data['words'].each do |word|
-          url = post.url
-          linked_once = false
+          words_with_urls << { 'word' => word, 'url' => post.url }
+        end
+      end
 
-          # Regex: शब्द को match करो भले वो <em>, <strong>, <b>, <i> tag के अंदर हो
-          # लेकिन <a ...> tag के अंदर मत पकड़ो
-          regex = /
-            (?<!["'>])                # <a href=""> वगैरह के अंदर मत पकड़ो
-            (?:<[^>]+>)*              # optional HTML formatting tag (<em>, <strong> etc.)
-            \b#{Regexp.escape(word)}\b
-            (?:<\/[^>]+>)*            # closing formatting tag
-            (?!["'<])                 # <a> या attribute के बाद मत पकड़ो
-          /x
+      # लंबाई के अनुसार reverse sort (लंबा पहले)
+      words_with_urls.sort_by! { |w| -w['word'].length }
 
-          input = input.gsub(regex) do |match|
-            if linked_once
-              match
-            else
-              linked_once = true
-              "<a href='#{url}'>#{match}</a>"
-            end
+      words_with_urls.each do |entry|
+        word = entry['word']
+        url = entry['url']
+        linked_once = false
+
+        # Regex: शब्द को match करो भले वो <em>, <strong>, <b>, <i> tag के अंदर हो
+        # लेकिन <a ...> tag के अंदर मत पकड़ो
+        regex = /
+          (?<!["'>])                # <a href=""> वगैरह के अंदर मत पकड़ो
+          (?:<[^>]+>)*              # optional HTML formatting tag (<em>, <strong> etc.)
+          \b#{Regexp.escape(word)}\b
+          (?:<\/[^>]+>)*            # closing formatting tag
+          (?!["'<])                 # <a> या attribute के बाद मत पकड़ो
+        /x
+
+        input = input.gsub(regex) do |match|
+          if linked_once
+            match
+          else
+            linked_once = true
+            "<a href='#{url}'>#{match}</a>"
           end
         end
       end
+
       input
     end
   end
